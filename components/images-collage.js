20b820f2260e1326998e4553e14f195977841f25
yl-br <yuvalbic@gmail.com>
2026-01-10 04:52:32 +0200

    Bla

--- components/images-collage.js
+++ components/images-collage.js
@@ -5,14 +5,14 @@ const ImagesCollage = {
   data() {
     return {
       images: [
-        '/assets/images/atomic-bomb.png',
-        '/assets/images/atomic-cloud.png',
-        '/assets/images/bird.png',
-        '/assets/images/bomb_ball.png',
-        '/assets/images/cow.png',
-        '/assets/images/dynamite.png',
-        '/assets/images/parrots.png',
-        '/assets/images/wind.png'
+        './assets/images/atomic-bomb.png',
+        './assets/images/atomic-cloud.png',
+        './assets/images/bird.png',
+        './assets/images/bomb_ball.png',
+        './assets/images/cow.png',
+        './assets/images/dynamite.png',
+        './assets/images/parrots.png',
+        './assets/images/wind.png'
       ]
     };
   },
